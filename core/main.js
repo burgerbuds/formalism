812ecc531378e4b27bf2e03488389c19a922d913
Ben Rogerson <info@benrogerson.com.au>
2019-03-26 11:18:34 +1030

    cleans

--- core/main.js
+++ core/main.js
@@ -75,7 +75,9 @@ const initFormalism = () => {
 // Element.closest() Polyfill for IE9+
 // https://developer.mozilla.org/en-US/docs/Web/API/Element/closest#Polyfill
 if (!Element.prototype.matches) {
-    Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+    Element.prototype.matches =
+        Element.prototype.msMatchesSelector ||
+        Element.prototype.webkitMatchesSelector;
 }
 
 if (!Element.prototype.closest) {
