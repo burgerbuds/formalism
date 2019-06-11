86d2099613c8d70934389a4835fdab0695165d0d
Ben Rogerson <info@benrogerson.com.au>
2019-06-11 13:44:20 +0930

    improve listeners

--- src/modules/field-filled-listeners.js
+++ src/modules/field-filled-listeners.js
@@ -26,32 +26,49 @@ const initFieldFilledListeners = (
     targetFields = ['INPUT', 'SELECT', 'TEXTAREA'],
     blacklistedTypes = [
         'button',
-        'checkbox',
         'color',
         'hidden',
         'image',
-        'radio',
         'range',
         'reset',
         'submit',
     ]
 ) => {
+    const SELECTOR = {
+        FIELD_CONTAINER: '[data-field-container]',
+    };
+
     const isSelectOneFilled = ({ options }) =>
         options.length === 0
             ? false
             : options[options.selectedIndex].label.length > 0;
+
+    const getContainer = field => {
+        // Get parent container
+        let container = field.closest(SELECTOR.FIELD_CONTAINER);
+        // Check if container is part of a fieldset
+        const isFieldset = container.hasAttribute('data-field-fieldgroup');
+        // Select the parent fieldset if field
+        if (isFieldset) {
+            container = container.parentNode.closest(SELECTOR.FIELD_CONTAINER);
+        }
+        if (!container) return console.warn(`No container found for ${field}`);
+        return container;
+    };
+
     const handleBlurEvent = ({ target }) => {
         let isFilled;
         // Treat select fields differently
         if (target.type === 'select-one') isFilled = isSelectOneFilled(target);
-        // Get the closest container ancestor
-        const container = target.closest('[data-container]');
+        // Get the container ancestor
+        const container = getContainer(target);
         if (!container) return;
         // Add or remove the is-filled class
         if (isFilled || target.value.length > 0)
             return container.classList.add('is-filled');
         container.classList.remove('is-filled');
     };
+
     // Add listeners for blur events on form fields
     Array.from(fields)
         .filter(field => targetFields.indexOf(field.nodeName) >= 0)
